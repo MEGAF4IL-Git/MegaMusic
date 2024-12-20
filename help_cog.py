11736257a08d8e5891dd11667db941cb5a1e56a4
MEGAF4IL <185027479+MEGAF4IL-Git@users.noreply.github.com>
2024-12-19 19:10:35 -0600

    Update help_cog.py

--- help_cog.py
+++ help_cog.py
@@ -12,16 +12,16 @@ class help_cog(commands.Cog):
         self.help_message = f"""
 ```
 General commands:
-{self.bot.command_prefix}help - displays all the available commands
-{self.bot.command_prefix}q - displays the current music queue
-{self.bot.command_prefix}p <YouTube Link> - finds the song on youtube and plays it in your current channel. Will resume playing the current song if it was paused
-{self.bot.command_prefix}skip - skips the current song being played
-{self.bot.command_prefix}clear - Stops the music and clears the queue
-{self.bot.command_prefix}stop - Disconnected the bot from the voice channel
-{self.bot.command_prefix}pause - pauses the current song being played or resumes if already paused
-{self.bot.command_prefix}resume - resumes playing the current song
-{self.bot.command_prefix}prefix - change command prefix
-{self.bot.command_prefix}remove - removes last song from the queue
+{self.bot.command_prefix}Help - Displays all the available commands.
+{self.bot.command_prefix}Q - Displays the current music queue.
+{self.bot.command_prefix}P <YouTube Link> - finds the song on youtube and plays it in your current channel. Will resume playing the current song if it was paused.
+{self.bot.command_prefix}Skip - Skips the current song being played.
+{self.bot.command_prefix}Clear - Stops the music and clears the queue.
+{self.bot.command_prefix}Stop - Disconnect the bot from the voice channel.
+{self.bot.command_prefix}Pause - Pauses the current song being played or resumes if already paused.
+{self.bot.command_prefix}Resume - Resumes playing the current song.
+{self.bot.command_prefix}Prefix - change command prefix.
+{self.bot.command_prefix}Remove - Removes most recently added song from the queue.
 ```
 """
 
